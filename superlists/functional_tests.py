003f9a95ff8b3dd05f5b857a158781d1631f6d10
alfawuhn <alfawuhn@gmail.com>
2016-01-19 23:14:31 -0500

    Function test now checks we can input a to-do

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -12,9 +12,37 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # I have heard about a new online to-do app.
+        # I go to check out it's homepage
         self.browser.get('http://localhost:8000')
 
+        # I notice that the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # I am invited to enter a to-do item right away
+        inputbox = self.browser.find_element_by_tag_name('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # I type "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When I hit enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # There is still a text box inviting me to add another item.
+        # I ender "Use peacock feathers to make a fly"
         self.fail('Finish the test!')
 
     def test_google(self):
